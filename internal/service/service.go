2c24a44ea7bd907981d0ab71299d5ea314838fc1
nicitapa <nicapagodev@gmail.com>
2025-09-29 19:07:29 +0300

    corrected errors

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,16 +2,19 @@ package service
 
 import (
 	"github.com/nicitapa/firstProgect/internal/contracts"
+	"github.com/rs/zerolog"
 )
 
 type Service struct {
 	repository contracts.RepositoryI
 	cache      contracts.CacheI
+	logger     zerolog.Logger
 }
 
-func NewService(repository contracts.RepositoryI, cache contracts.CacheI) *Service {
+func NewService(repository contracts.RepositoryI, cache contracts.CacheI, logger zerolog.Logger) *Service {
 	return &Service{
 		repository: repository,
 		cache:      cache,
+		logger:     logger,
 	}
 }
