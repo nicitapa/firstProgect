c211d6943f7643a3daf5c4086bebbd63e7d1c6f5
nicitapa <nicapagodev@gmail.com>
2025-09-29 19:18:09 +0300

    corrected errors

--- internal/repository/employees.go
+++ internal/repository/employees.go
@@ -3,14 +3,18 @@ package repository
 import (
 	"context"
 	"github.com/nicitapa/firstProgect/internal/models"
+	"github.com/rs/zerolog"
+	"os"
 )
 
 func (r *Repository) GetAllEmployees(ctx context.Context) (employees []models.Employees, err error) {
+	logger := zerolog.New(os.Stdout).With().Timestamp().Str("func_name", "repository.GetAllProducts").Logger()
 
 	if err = r.db.SelectContext(ctx, &employees, `
 		SELECT id, name, email, age, 
 		FROM employees
 		ORDER BY id`); err != nil {
+		logger.Err(err).Msg("error selecting employees")
 		return nil, r.translateError(err)
 	}
 
@@ -19,6 +23,7 @@ func (r *Repository) GetAllEmployees(ctx context.Context) (employees []models.Em
 }
 
 func (r *Repository) GetEmployeesByID(ctx context.Context, id int) (employees models.Employees, err error) {
+
 	if err = r.db.GetContext(ctx, &employees, `
 		SELECT id, name, email, age
 		FROM employees
@@ -30,19 +35,22 @@ func (r *Repository) GetEmployeesByID(ctx context.Context, id int) (employees mo
 }
 
 func (r *Repository) CreateEmployees(ctx context.Context, employees models.Employees) (err error) {
+
+	logger := zerolog.New(os.Stdout).With().Timestamp().Str("func_name", "repository.CreatEmplotees").Logger()
 	_, err = r.db.ExecContext(ctx, `INSERT INTO employees (name, email, age)
 					VALUES ($1, $2, $3)`,
 		employees.Name,
 		employees.Email,
 		employees.Age)
 	if err != nil {
+		logger.Err(err).Msg("error inserting employees")
 		return r.translateError(err)
 	}
-
 	return nil
 }
 
 func (r *Repository) UpdateEmployeesByID(ctx context.Context, employees models.Employees) (err error) {
+	logger := zerolog.New(os.Stdout).With().Timestamp().Str("func_name", "repository.UpdateByEmployeesID").Logger()
 	_, err = r.db.ExecContext(ctx, `
 		UPDATE employees SET name = $1, 
 		                    email = $2, 
@@ -53,17 +61,17 @@ func (r *Repository) UpdateEmployeesByID(ctx context.Context, employees models.E
 		employees.Age,
 		employees.ID)
 	if err != nil {
+		logger.Err(err).Msg("error updating employees")
 		return r.translateError(err)
-	}
-
 	return nil
 }
 
 func (r *Repository) DeleteEmployeesByID(ctx context.Context, id int) (err error) {
+		logger := zerolog.New(os.Stdout).With().Timestamp().Str("func_name", "repository.DeleteProductByID").Logger()
 	_, err = r.db.ExecContext(ctx, `DELETE FROM employees WHERE id = $1`, id)
-	if err != nil {
-		return r.translateError(err)
-	}
-
+		if err != nil {
+			logger.Err(err).Msg("error deleting product")
+			return r.translateError(err)
+		}
 	return nil
 }
